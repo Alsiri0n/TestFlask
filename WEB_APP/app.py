05afd58633769349bb4b7ad96d39223abf9518b1
Ignatev Nikita <ignatev@medilon.ru>
2022-10-04 17:38:13 +0300

    Testing deploying

--- WEB_APP/app.py
+++ WEB_APP/app.py
@@ -1,6 +1,15 @@
+import os
 from flask import Flask
+
+FLASK_HOST = str(os.environ.get('flask_host'))
+FLASK_PORT = int(os.environ.get('flask_port'))
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return 'ok\n'
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run(host= FLASK_HOST, port= FLASK_PORT)
