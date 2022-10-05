049089741a708199a6933a3dddbcfb8efe97f44d
Ignatev Nikita <ignatev@medilon.ru>
2022-10-05 17:42:23 +0300

    Add base template

--- WEB_APP/app.py
+++ WEB_APP/app.py
@@ -2,28 +2,28 @@
 Test application for testing CI/CD
 """
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 FLASK_HOST = str(os.environ.get('flask_host'))
 FLASK_PORT = int(os.environ.get('flask_port'))
 
 app = Flask(__name__)
 
-
+myMenu = ['Установка', 'Первое приложение', 'Обратная связь']
 @app.route('/index')
 @app.route('/')
 def index():
     """
     Main page of the site
     """
-    return 'ok\n'
+    return render_template('index.html', menu=myMenu)
 
 @app.route('/about')
 def about():
     """
     About page of the site
     """
-    return "<h1>О сайте</h1>"
+    return render_template('about.html', title='About', menu=myMenu)
 
 
 if __name__ == "__main__":
